c8044eeeea34b929921262732378493c6ce1bab7
dohyu <dohyunre702@gmail.com>
2022-12-13 13:15:13 +0900

    first commit

--- src/main/java/com/example/hello/SpringbootEssentialApplication.java
+++ src/main/java/com/example/hello/SpringbootEssentialApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootEssentialApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootEssentialApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEssentialApplication.class, args);
+    }
 
 }
